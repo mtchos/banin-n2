abd608ba6de5896e2af3cabb275045cdc9bbc0cb
mtchos <matheussilvaesousa@gmai.com>
2022-11-21 20:39:29 -0300

    Melhorar N2A

--- N2A.py
+++ N2A.py
@@ -11,28 +11,21 @@ arquivo = open("vendas.txt")
 # indice 0 -> codigos de produto
 # indice 1 -> quantidade de produto
 # indice 2 -> preço unitário de cada produto
-produtos = [[], [], []]
+produtos = []
 
 for linha in arquivo:
     registro = linha.split(sep=";")
     codigo = int(registro[0])
-    produtos[0].append(codigo)
     quantidade = int(registro[1])
-    produtos[1].append(quantidade)
     preco_unitario = float(registro[2])
-    produtos[2].append(preco_unitario)
+    produtos.append([codigo, quantidade, preco_unitario])
 
 arquivo.close()
 
-# para evitar alterações, transformar listas internas em tuplas
-produtos[0] = tuple(produtos[0])
-produtos[1] = tuple(produtos[1])
-produtos[2] = tuple(produtos[2])
-
 # PARTE 2 - fazer as contas do total geral
 total = 0
-for i in range(len(produtos[0])):
-    total += produtos[1][i] * produtos[2][i]
+for produto in produtos:
+    total += produto[1] * produto[2]
 
 print(f"O total geral vendido é R$ {total:.2f}\n\n")
 
@@ -42,9 +35,9 @@ while codigo != 0:
     codigo = int(input("Digite o código: "))
     if 10000 <= codigo <= 21000:
         total_produto = 0
-        for i in range(len(produtos[0])):
-            if produtos[0][i] == codigo:
-                total_produto += produtos[1][i] * produtos[2][i]
+        for produto in produtos:
+            if produto[0] == codigo:
+                total_produto += produto[1] * produto[2]
         print(f"Total vendido do produto {codigo} = R$ {total_produto:.2f}\n")
     elif codigo != 0:
         print(f"{codigo} Código inválido (deve ser entre 10000 de e 21000)\n")
